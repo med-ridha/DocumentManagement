28962ac9ecbe8cd082bd42dd48138d7fde9854ec
med-ridha <ridha.zemzem24@gmail.com>
2021-12-16 19:44:58 +0100

    initial setup

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DocumentListComponent } from './pages/document-list/document-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DocumentListComponent  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DocumentListComponent } from './pages/document-list/document-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DocumentListComponent
   ],
   imports: [
     BrowserModule,
